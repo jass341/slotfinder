a6ae4a64b203d2ab5630e3bbf83e66407df94eb6
dhruv <dhruvsharma1904@gmail.com>
2021-05-03 00:19:12 +0530

    Adding Style to the application

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('tables.html')
 
 @app.route('/api/get_slots')
 def hello_world():
